7f7dd54340a76f2acdc6d926844c79fec947ee10
Nicolas Soares Henrique <nicolas.henrique@foursys.com.br>
2024-11-25 09:33:28 -0300

    implementations finished

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -6,12 +6,12 @@ import {
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from '../../shared/models/course';
-import { Observable } from 'rxjs';
-import { Lesson } from '../../shared/models/lesson';
-import { concatMap, tap } from 'rxjs/operators';
+import { map, switchMap, tap, withLatestFrom } from 'rxjs/operators';
 import { AngularMaterialModule } from '../../shared/Material.module';
 import { CommonModule } from '@angular/common';
-import { CoursesHttpService } from '../../services/courses-http.service';
+import { CourseEntityService } from '../../services/course-entity.service';
+import { LessonEntityService } from '../../services/lesson-entity.service';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'course',
@@ -22,26 +22,52 @@ import { CoursesHttpService } from '../../services/courses-http.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseComponent implements OnInit {
-  private readonly coursesService = inject(CoursesHttpService);
   private readonly route = inject(ActivatedRoute);
+  private readonly courseService = inject(CourseEntityService);
+  private readonly lessonService = inject(LessonEntityService);
 
-  course$: Observable<Course>;
-  lessons$: Observable<Lesson[]>;
-  loading$: Observable<boolean>;
+  course = toSignal(
+    this.route.params.pipe(
+      switchMap((params) =>
+        this.courseService.entities$.pipe(
+          map((courses) =>
+            courses.find((course) => course.url == params['courseUrl'])
+          )
+        )
+      )
+    )
+  );
+
+  lessons = toSignal(
+    this.lessonService.entities$.pipe(
+      withLatestFrom(toObservable(this.course)),
+      tap(([_, course]) => {
+        if (this.nextPage == 0) {
+          this.loadLessonsPage(course);
+        }
+      }),
+      map(([lessons, course]) =>
+        lessons.filter((lesson) => lesson.courseId == course.id)
+      )
+    )
+  );
+
+  loading = toSignal(this.lessonService.loading$);
 
   displayedColumns = ['seqNo', 'description', 'duration'];
   nextPage = 0;
 
   ngOnInit() {
-    const courseUrl = this.route.snapshot.paramMap.get('courseUrl');
-
-    this.course$ = this.coursesService.findCourseByUrl(courseUrl);
-
-    this.lessons$ = this.course$.pipe(
-      concatMap((course) => this.coursesService.findLessons(course.id)),
-      tap(console.log)
-    );
+    this.loadLessonsPage(this.course());
   }
 
-  loadLessonsPage(course: Course) {}
+  loadLessonsPage(course: Course) {
+    this.lessonService.getWithQuery({
+      courseId: course.id,
+      pageNumber: this.nextPage,
+      pageSize: 3,
+    });
+
+    this.nextPage += 1;
+  }
 }
